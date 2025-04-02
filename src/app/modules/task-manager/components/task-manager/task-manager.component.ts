4640a7aeb9594725946a9aa6d8096dace6702a77
FaAzI Ahamed <faaziahamed@gmail.com>
2025-04-02 16:46:29 +0530

    Cleanup

--- src/app/modules/task-manager/components/task-manager/task-manager.component.ts
+++ src/app/modules/task-manager/components/task-manager/task-manager.component.ts
@@ -24,8 +24,6 @@ export class TaskManagerComponent implements OnInit {
 
   ngOnInit() {
     this.tasks = this.taskService.getTasks();
-
-    // this.loadTasks();
   }
 
   loadTasks() {
@@ -35,58 +33,7 @@ export class TaskManagerComponent implements OnInit {
     }
   }
 
-  // saveTasks() {
-  //   localStorage.setItem('tasks', JSON.stringify(this.tasks));
-  // }
-
-  // addTask() {
-  //   if (!this.newTaskTitle.trim()) return;
-  //   const newTask: Task = {
-  //     id: Date.now(),
-  //     title: this.newTaskTitle,
-  //     status: Status.NotDone,
-  //     priority: this.newTaskPriority
-  //   };
-  //   this.tasks.push(newTask);
-  //   this.saveTasks();
-  //   this.snackBar.open('Task Added!', 'Close', { duration: 2000 });
-  //   this.newTaskTitle = '';
-  //   this.newTaskPriority = Priority.Low;
-  // }
-
-  // deleteTask(id: number) {
-  //   this.tasks = this.tasks.filter(task => task.id !== id);
-  //   this.saveTasks();
-  //   this.snackBar.open('Task Deleted!', 'Close', { duration: 2000 });
-  // }
-
-  // toggleStatus(id: number) {
-  //   const task = this.tasks.find(task => task.id === id);
-  //   if (task) {
-  //     task.status = task.status === Status.Done ? Status.NotDone : Status.Done;
-  //     this.saveTasks();
-  //     this.snackBar.open('Task Updated!', 'Close', { duration: 2000 });
-  //   }
-  // }
-
   get filteredTasks() {
-    console.log(
-      this.tasks
-        .filter((task) =>
-          task.title.toLowerCase().includes(this.searchQuery.toLowerCase())
-        )
-        .sort((a, b) => {
-          if (this.sortBy === 'priority') {
-            // Compare the enum values directly
-            return (
-              Object.values(Priority).indexOf(a.priority as any) -
-              Object.values(Priority).indexOf(b.priority as any)
-            );
-          } else {
-            return Number(a.status) - Number(b.status);
-          }
-        })
-    );
     return this.tasks
       .filter((task) =>
         task.title.toLowerCase().includes(this.searchQuery.toLowerCase())
